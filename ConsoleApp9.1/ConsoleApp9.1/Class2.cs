23892b4315386c527fcf2ad031cc4fd6712aefd4
Karls0n <85025692+Shapochka2005@users.noreply.github.com>
2022-12-07 15:50:58 +0300

    Add files via upload

---
+++ ConsoleApp9.1/ConsoleApp9.1/Class2.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9._1
+{
+    public static class Extensions
+    {
+        public static void RemoveFirst<T>(this List<T> list)
+        {
+            if (list.Any())
+            {
+                list.Remove(list.First());
+            }
+        }
+
+        public static void RemoveLast<T>(this List<T> list)
+        {
+            if (list.Any())
+            {
+                list.Remove(list.Last());
+            }
+        }
+
+        public static Point Copy(this Point point)
+        {
+            return new Point(point.X, point.Y);
+        }
+
+        public static void Write(this IEnumerable<Point> points, string text)
+        {
+            foreach (var point in points)
+            {
+                Write(text, point.X, point.Y);
+            }
+        }
+
+        public static void Write(this IEnumerable<Point> points, string text, ConsoleColor foreground, ConsoleColor background)
+        {
+            foreach (var point in points)
+            {
+                Write(text, point.X, point.Y, foreground, background);
+            }
+        }
+
+        public static void Write(this Point point, string text)
+        {
+            Write(text, point.X, point.Y);
+        }
+
+        public static void Write(this Point point, string text, ConsoleColor foreground, ConsoleColor background)
+        {
+            Write(text, point.X, point.Y, foreground, background);
+        }
+
+        public static void Write(this string text, int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(text);
+        }
+
+        public static void Write(this string text, int x, int y, ConsoleColor foreground, ConsoleColor background)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.ForegroundColor = foreground;
+            Console.BackgroundColor = background;
+            Console.Write(text);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ ConsoleApp9.1/ConsoleApp9.1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+
+namespace Snake
+{
+    public enum Direction { Stop, Up, Down, Left, Right, X, Y }
+
+}
